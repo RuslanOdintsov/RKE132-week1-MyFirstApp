be77a01e7f82d83755e1300d411a93641f10d94e
Ruslan.Odintsov <rodintso@tktk.ee>
2024-01-30 23:07:52 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿//This is comment to my code.:))
+
+//Rakendus küsib kasutajal sisestada oma nime
+//Rakendus nimetab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");//output
+
+//string-sõne
+string userName = Console.ReadLine();//input
+Console.WriteLine("Hello" + "," + userName + "!");//output
+
+Console.WriteLine($"Hello,{userName}!");//output
